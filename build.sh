1aca80cc393ec27df63886f0e3174faad3afb191
Dj-Viking <anders.swedishviking@gmail.com>
2026-06-11 23:01:46 -0400

    neaten up the code and adjust the build script

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #! /usr/bin/bash
+
 MONITOR=$1
 
+# build the rust project to prepare flashing esp with the compiled rust code
 cargo build
 
-doas espflash flash ./target/riscv32imac-unknown-none-elf/debug/rust-esp-neopixel
-
+# kind of a weird way to check if we want to do monitor or not
+# this may need to be adjusted for other operating systems that don't have `doas`
+# because I think this requires administrative access to run the flash
 if ! [ -z "$MONITOR" ]; then
 	if [ "$MONITOR" = "-m" ]; then
+		# TODO: check the monitor warning, im not sure if the monitor quite works...
+		doas espflash flash ./target/riscv32imac-unknown-none-elf/debug/rust-esp-neopixel
 		doas espflash monitor
 	fi
+else
+	doas espflash flash ./target/riscv32imac-unknown-none-elf/debug/rust-esp-neopixel
 fi
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -21,13 +21,10 @@ use esp_hal::usb_serial_jtag::UsbSerialJtag;
 use core::fmt::Write;
 use embedded_io_async::Read;
 
-fn blink_once(led: &mut Output<'_>, delay: &mut Delay) {
-    led.set_high();
-    delay.delay_millis(10);
-    led.set_low();
-    delay.delay_millis(10);
-}
 
+// if the main function panics during monitoring `sh build.sh -m`
+// (if monitoring works), the panic message will be sent to the console through the usb JTAG back
+// to the computer terminal from the esp 
 #[panic_handler]
 fn panic(p: &core::panic::PanicInfo) -> ! {
     let mut usb = UsbSerialJtag::new(unsafe { esp_hal::peripherals::USB_DEVICE::steal() });
@@ -49,81 +46,59 @@ const LED_COUNT: usize = 2;
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
 
-	// let mut delay = Delay::new();
+    let peripherals = esp_hal::init(
+        esp_hal::Config::default()
+            .with_cpu_clock(CpuClock::max()));
 
-    // let peripherals = esp_hal::init(
-    //     esp_hal::Config::default()
-    //         .with_cpu_clock(CpuClock::max()));
+	// I don't remember what this is for :)
+    // esp_hal_embassy::init(SystemTimer::new(peripherals.SYSTIMER).alarm0);
 
-    // // esp_hal_embassy::init(SystemTimer::new(peripherals.SYSTIMER).alarm0);
+	// pins [according to that DRV8825 motor module - 
+	// pins will have to change if we change the
+	// motor module we wanna use
+	let _enable = Output::new(peripherals.GPIO5,
+        		esp_hal::gpio::Level::High,
+				OutputConfig::default());
 
-    // // let mut usb_async = UsbSerialJtag::new(peripherals.USB_DEVICE).into_async();
+		// step degree subdivisions
+	let mut m0    = peripherals.GPIO6;
+	let mut m1    = peripherals.GPIO7;
+	let mut m2    = peripherals.GPIO8;
+		// step degree subdivisions
 
-	// let _enable = Output::new(peripherals.GPIO5,
-    //     		esp_hal::gpio::Level::High,
-	// 			OutputConfig::default());
+	let mut step  = peripherals.GPIO0;
+	
+	let mut reset = peripherals.GPIO9;
+	let mut dir   = peripherals.GPIO1;
+	let mut slp   = peripherals.GPIO4;
+	// pins 
 
-	// let mut m0   = peripherals.GPIO6;
-	// let mut m1   = peripherals.GPIO7;
-	// let mut m2   = peripherals.GPIO8;
+	// outputs
+	let op0             = Output::new(m0,   Level::High, OutputConfig::default());
+	let op1             = Output::new(m1,   Level::Low,  OutputConfig::default());
+	let op2             = Output::new(m2,   Level::Low,  OutputConfig::default());
 
-	// let mut rst  = peripherals.GPIO9;
-	// let mut step = peripherals.GPIO0;
-	// let mut dir  = peripherals.GPIO1;
-	// let mut slp  = peripherals.GPIO4;
+	let oprst           = Output::new(reset,  Level::Low,  OutputConfig::default());
+	let opdir           = Output::new(dir,  Level::Low,  OutputConfig::default());
+	let opslp           = Output::new(slp,  Level::Low,  OutputConfig::default());
 
-	// let op0        = Output::new(m0,   Level::High, OutputConfig::default());
-	// let op1        = Output::new(m1,   Level::Low,  OutputConfig::default());
-	// let op2        = Output::new(m2,   Level::Low,  OutputConfig::default());
+	let mut step_output = Output::new(step, Level::High, OutputConfig::default());
+	// outputs
 
-	// let oprst      = Output::new(rst,  Level::Low,  OutputConfig::default());
-	// let mut opstep = Output::new(step, Level::High, OutputConfig::default());
-	// let opdir      = Output::new(dir,  Level::Low,  OutputConfig::default());
-	// let opslp      = Output::new(slp,  Level::Low,  OutputConfig::default());
+	let mut delay       = Delay::new();
 
-	// data is a sequence of 24bit GRB colors, as [u8; _]
-	// start off with some green
-	let mut data: [u8; LED_COUNT*3] = [
-	//   G  R  B
-		20, 0, 0, // led 0
-		0,  0, 20  // led 1
-		// ... 4096
-	];
-
-	// let mut usb_buf: [u8; LED_COUNT*3] = [0; _];
-
-	// spawner.spawn(task()).unwrap();
 
 	loop {
-		// if swapping to reading from this usb to not read from usb
-		// then you have to disconnect IO9 and then reset it after plugging it in
-		// to power again
-
-		// usb_async.read_exact(&mut usb_buf).await.unwrap();
-
-		// let mut pulses: [u32; LED_COUNT*24 + 1] = [0; _];
-
-		// data.map(byte_to_pulses).iter().enumerate().for_each(|(i, e)|
-		// 	pulses[i * 8..(i + 1) * 8].copy_from_slice(e));
-		// usb_buf.map(byte_to_pulses).iter().enumerate().for_each(|(i, e)|
-		// 	pulses[i * 8..(i + 1) * 8].copy_from_slice(e));
-
-		// pulses[pulses.len()-1] = PulseCode::new(Level::Low, 0, Level::Low, 0);
-		// channel.transmit(&pulses).await.unwrap();
-
-		// task() in the background
-		//
-
-		// Timer::after_millis(20).await;
-
-		// blink_once(&mut opstep, &mut delay);
-
-
-		// fades from 0 blue to max blue until it wraps around to 0 again
-		// data[2] = data[2].wrapping_add(10);
+		step_on_delay(&mut step_output, &mut delay);
 	}
 }
 
+fn step_on_delay(step_output: &mut Output<'_>, delay: &mut Delay) {
+    step_output.set_high();
+    delay.delay_millis(10);
+    step_output.set_low();
+    delay.delay_millis(10);
+}
 
 fn byte_to_pulses(b: u8) -> [u32; 8] {
     [7, 6, 5, 4, 3, 2, 1, 0].map(|i| match (b >> i) & 1 {
